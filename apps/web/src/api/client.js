e77d7d8bae9e77265e1e12145bb497fa40736c6c
ArmV17 <22040111@alumno.utc.ude.mx>
2025-11-24 19:19:54 -0600

    Cambio de App

--- apps/web/src/api/client.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const api = axios.create({ baseURL: 'http://localhost:4000/api' })
-export function setAuth(){
-  const t = localStorage.getItem('token')
-  if (t) api.defaults.headers.common['Authorization'] = `Bearer ${t}`
-  else delete api.defaults.headers.common['Authorization']
-}
-export default api
